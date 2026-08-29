773560f1980df5e7e1b223d5fa815d4cc2037c82
Chigui <denischigui16@gmail.com>
2026-08-28 23:23:46 -0600

    Actualiza cache de seleccion de club

--- js/intro-entry.js
+++ js/intro-entry.js
@@ -161,5 +161,5 @@ if (invitationContinueButton) {
 
 // Pantalla interactiva de elección del primer club.
 const clubSelectionScript = document.createElement('script');
-clubSelectionScript.src = 'js/club-selection.js?v=2.1.1';
+clubSelectionScript.src = 'js/club-selection.js?v=2.1.2';
 document.body.appendChild(clubSelectionScript);
